5c2e66e20e328aa1a8d099167e559d63f5ade7f7
yohanespnt <mmyohanes@gmail.com>
2021-11-26 15:58:25 -0600

    second time commit

--- eclipse-workspace/Automationtraining/src/main/java/peopleNtech/Automationtraining/Iborowserworking.java
+++ eclipse-workspace/Automationtraining/src/main/java/peopleNtech/Automationtraining/Iborowserworking.java
@@ -1,18 +1,19 @@
 package peopleNtech.Automationtraining;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Iborowserworking {
 
 	static WebDriver driver;
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args, By by, String email) throws InterruptedException {
 		//how to open the browser
 		
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\mmyoh\\eclipse-workspace\\Automationtraining\\src\\drivers\\chromedriver.exe");
 		
 		driver= new ChromeDriver();
-		driver.navigate().to("https://www.amazon.com");
+		driver.navigate().to("https://www.facebook.com");
 		driver.manage().window().maximize();
 		Thread.sleep(9000);
 		driver.navigate().refresh();
@@ -24,8 +25,7 @@ public class Iborowserworking {
 		System.out.println(windowhandle);
 		String currenturl=driver.getCurrentUrl();
 		System.out.println(currenturl);
-		driver.close();
-		driver.quit();
+		driver.findElement(by.id(email));
 		
 		
 		
