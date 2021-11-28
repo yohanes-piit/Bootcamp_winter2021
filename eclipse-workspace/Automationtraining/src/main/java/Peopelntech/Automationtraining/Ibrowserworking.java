d588d0eeac7e0f0e7aa02ad49c41ac23eabfa338
yohanespnt <mmyohanes@gmail.com>
2021-11-27 20:31:41 -0600

    my first commit

---
+++ eclipse-workspace/Automationtraining/src/main/java/Peopelntech/Automationtraining/Ibrowserworking.java
@@ -0,0 +1,34 @@
+package Peopelntech.Automationtraining;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Ibrowserworking {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+System.setProperty("webdriver.chrome.driver", "C:\\Users\\mmyoh\\eclipse-workspace\\Automationtraining\\src\\drivers\\chromedriver.exe");
+WebDriver driver= new ChromeDriver();
+Thread.sleep(2000);
+driver.get("https://www.amazon.com");
+driver.manage().window().maximize();
+Thread.sleep(2000);
+driver.navigate().refresh();
+Thread.sleep(2000);
+driver.navigate().back();
+Thread.sleep(2000);
+driver.navigate().forward();
+Thread.sleep(2000);
+String windowhandle = driver.getWindowHandle();
+System.out.println(windowhandle);
+Thread.sleep(2000);
+String url=driver.getCurrentUrl();
+System.out.println(url);
+driver.close();
+
+
+
+
+	}
+
+}
